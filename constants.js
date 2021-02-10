47144e79d8495fd3e7f19ae229b6bbf55b52435f
t.claude94 <23203061+tclaude94@users.noreply.github.com>
2021-02-10 22:41:04 +0100

    Add Jetbrains Tools with their icons

--- constants.js
+++ constants.js
@@ -8,3 +8,51 @@ const ICONS = {
   github: getFullIconURL("github-icon.png"),
   vscode: getFullIconURL("vscode-icon.png"),
 };
+
+const JETBRAINS_TOOLS = {
+  appcode: {
+    name: "AppCode",
+    tag: "appcode",
+    icon: getFullIconURL("appcode-icon.png"),
+  },
+  clion: {
+    name: "CLion",
+    tag: "clion",
+    icon: getFullIconURL("clion-icon.png"),
+  },
+  goland: {
+    name: "GoLand",
+    tag: "goland",
+    icon: getFullIconURL("goland-icon.png"),
+  },
+  idea: {
+    name: "IntelliJ IDEA",
+    tag: "idea",
+    icon: getFullIconURL("idea-icon.png"),
+  },
+  phpstorm: {
+    name: "PhpStorm",
+    tag: "php-storm",
+    icon: getFullIconURL("phpstorm-icon.png"),
+  },
+  pycharm: {
+    name: "PyCharm",
+    tag: "pycharm",
+    icon: getFullIconURL("pycharm-icon.png"),
+  },
+  rider: {
+    name: "Rider",
+    tag: "rd",
+    icon: getFullIconURL("rider-icon.png"),
+  },
+  rubymine: {
+    name: "RubyMine",
+    tag: "rubymine",
+    icon: getFullIconURL("rubymine-icon.png"),
+  },
+  webstorm: {
+    name: "WebStorm",
+    tag: "web-storm",
+    icon: getFullIconURL("webstorm-icon.png"),
+  },
+};
